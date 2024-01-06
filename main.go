ebc420e86d7b65ce43bab9616056ace7b38c11d5
n1h41 <nihalninu25@gmail.com>
2024-01-06 15:52:50 +0530

    implemented list uploaded files for download

--- main.go
+++ main.go
@@ -2,12 +2,16 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net/http"
+	"nihal-innsof/file-upload/templates/components"
 	"nihal-innsof/file-upload/templates/layout"
 	"os"
 
 	"github.com/a-h/templ"
 )
+
+const downloadDirectory = "./uploads/"
 
 func main() {
 	index := layout.Index()
@@ -23,11 +27,26 @@ func main() {
 			fmt.Println("File recieved")
 			fmt.Println("File name:", fileName)
 		}
-		f, err := os.Open("./uploads/fileName")
+		f, err := os.Create("./uploads/" + fileName)
 		if err != nil {
 			panic(err)
 		}
 		defer f.Close()
+		io.Copy(f, file)
+		successComponent := components.Response("Upload success")
+		successComponent.Render(r.Context(), w)
 	})
-	http.ListenAndServe(":8080", nil)
+	http.HandleFunc("/downloads", func(w http.ResponseWriter, r *http.Request) {
+		entries, err := os.ReadDir(downloadDirectory)
+		var resultList []string
+		if err != nil {
+			panic(err)
+		}
+		for _, item := range entries {
+			resultList = append(resultList, item.Name())
+		}
+		itemList := components.ItemList(resultList)
+		itemList.Render(r.Context(), w)
+	})
+	http.ListenAndServe("localhost:8080", nil)
 }
